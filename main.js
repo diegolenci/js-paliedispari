c2f2da7791a1115f479fd30dd2d8aeaa991aea76
diegolenci <lencidiego0@gmail.com>
2025-01-15 16:06:19 +0100

    esercizio 2: variabili e funzione

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma*/
 
 //RICHIESTA PAROLA A UTENTE
-let parola1 = prompt("inserisci una parola")
+/*let parola1 = prompt("inserisci una parola")
 // FUNZIONE CON VARIABILE PER CONVERTIRE STRINGA IN ARRAY, ROVESCIAROLO E CONVERTIRE ANCORA IN STRINGA
 function Palindromo(parola1) {
     let rev = parola1.split("").reverse().join("");
@@ -16,7 +16,7 @@ function Palindromo(parola1) {
     
 }
 //OUTPUT
-console.log(Palindromo(parola1));
+console.log(Palindromo(parola1));*/
 
 
 
@@ -25,6 +25,19 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 Dichiariamo chi ha vinto.*/
- 
+
+//DOMANDE PER UTENTE
+let pariOPdispari = prompt("pari o dispari?")
+let numero = parseInt(prompt("secgli numero da 1 a 5"))
+
+console.log(`i dati scelti dall'utente sono: ${pariOPdispari} ${numero}`)
+
+function numeriRandom(){
+    let random = Math.floor(Math.random() * 5) * 1;
+    return random
+}
+
+
+
 
 
